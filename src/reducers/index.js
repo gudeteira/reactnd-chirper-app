592a7f6116c6febf288284816bac64b4aa54d2c4
pedro.gude <gudeteira@gmail.com>
2019-02-27 00:31:36 +0100

    feat: Created reducers and combine it

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux'
+import authedUser from './authedUser'
+import tweets from './tweets'
+import users from './users'
+
+export default combineReducers({
+  authedUser,
+  users,
+  tweets
+})
