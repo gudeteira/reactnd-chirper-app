4da778c6735540352288d18638113981b4c3f1a7
pedro.gude <gudeteira@gmail.com>
2019-02-27 01:05:43 +0100

    feat: Load initial data and created Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className='center'>Your Timeline</h3>
+        <ul className='dahsboard-list'>
+          {this.props.tweets.map(id => (
+            <li key={id}>
+              <div>
+                TWEET_ID: {id}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({tweets}) {
+  return {
+    tweets: Object.keys(tweets)
+      .sort((a, b) => tweets[b].timestamp - tweets[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
